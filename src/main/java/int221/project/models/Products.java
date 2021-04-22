cccc556eb2ef4d66d3996c5957d159fdfa86b13f
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-04-22 10:59:01 +0700

    Making Products class to be an Entity Class

--- src/main/java/int221/project/models/Products.java
+++ src/main/java/int221/project/models/Products.java
@@ -1,5 +1,87 @@
 package int221.project.models;
 
-public class Products {
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Products
+{
+	@Id
+    private String     productcode  ; // Id or Primary Key
+
+    private String name ;
+    private String desciption ;
+    private double price ;
+    private Date date ;
+    private String url ;
+    private double warranty ;
+    private int brandid ;
+    
+
+	public void setProductcode( String productcode ) {
+        this.productcode = productcode ;
+    }
+
+	public String getProductcode() {
+        return this.productcode;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setDesciption( String desciption ) {
+        this.desciption = desciption;
+    }
+
+    public String getDesciption() {
+        return this.desciption;
+    }
+
+    public void setPrice( double price ) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setDate( Date date ) {
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setUrl( String url ) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setWarranty( double warranty ) {
+        this.warranty = warranty;
+    }
+
+    public double getWarranty() {
+        return this.warranty;
+    }
+
+    public void setBrandid( int brandid ) {
+        this.brandid = brandid;
+    }
+
+    public int getBrandid() {
+        return this.brandid;
+    }
 
 }
