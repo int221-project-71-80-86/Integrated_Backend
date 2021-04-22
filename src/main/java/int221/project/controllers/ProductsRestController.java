07cded0d11fba3564b446b5144b9614284aefdd6
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-04-22 11:02:13 +0700

    Add GET routing path to test database connection.

--- src/main/java/int221/project/controllers/ProductsRestController.java
+++ src/main/java/int221/project/controllers/ProductsRestController.java
@@ -1,14 +1,31 @@
 package int221.project.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import int221.project.models.Products;
 import int221.project.repositories.ProductsJpaRepository;
+
 
 @RestController
 public class ProductsRestController {
 	
 	@Autowired
 	ProductsJpaRepository repo;
+	
+    @GetMapping("/products/{code}")
+    public Products show(@PathVariable String code) {
+
+        return repo.findById(code).orElse(null);
+    }
+    
+    @GetMapping("/products")
+    public List<Products> showAll(){
+    	return repo.findAll();
+    }
 	
 }
