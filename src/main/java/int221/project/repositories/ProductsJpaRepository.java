86db6f167316c304e0834ebdf735143584d4ef98
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-04-30 12:48:37 +0700

    Make a test query.

--- src/main/java/int221/project/repositories/ProductsJpaRepository.java
+++ src/main/java/int221/project/repositories/ProductsJpaRepository.java
@@ -1,8 +1,13 @@
 package int221.project.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import int221.project.models.Products;
 
-public interface ProductsJpaRepository extends JpaRepository<Products, String>{
+public interface ProductsJpaRepository extends JpaRepository<Products, Integer>{
 	
+//	@Query("SELECT p FROM Products p WHERE p.name = ?1")
+//	Products findProductByProductName(String name);
+
 }
