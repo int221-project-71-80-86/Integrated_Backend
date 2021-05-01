769af02f8744cc6a15ea55f67d99f176bd177803
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-05-01 14:30:30 +0700

    Test something but not implemented.

--- src/main/java/int221/project/ProjectApplication.java
+++ src/main/java/int221/project/ProjectApplication.java
@@ -1,13 +1,27 @@
 package int221.project;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import int221.project.services.FileStoreServices;
+
 @SpringBootApplication
+//public class ProjectApplication implements CommandLineRunner {
 public class ProjectApplication {
+//	@Resource
+//	FileStoreServices storeService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
+	
+//	@Override
+//	  public void run(String... arg) throws Exception {
+//	    storeService.deleteAll();
+//	    storeService.init();
+//	  }
 
 }
