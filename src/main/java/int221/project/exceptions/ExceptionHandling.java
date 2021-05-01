7b4aa29c0a71cd5ad4ad94df444988321d9f28cc
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-05-01 13:38:22 +0700

    Add new ExceptionHandler to handling upload file more than specify size.

--- src/main/java/int221/project/exceptions/ExceptionHandling.java
+++ src/main/java/int221/project/exceptions/ExceptionHandling.java
@@ -7,7 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import int221.project.files.ResponseMessage;
 
 @ControllerAdvice
 public class ExceptionHandling extends ResponseEntityExceptionHandler {
@@ -21,4 +24,9 @@ public class ExceptionHandling extends ResponseEntityExceptionHandler {
 		 return entity;
 		 
 	 }
+	 
+	 @ExceptionHandler(MaxUploadSizeExceededException.class)
+	  public ResponseEntity<ResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+	    return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage("File too large!"));
+	  }
 }
