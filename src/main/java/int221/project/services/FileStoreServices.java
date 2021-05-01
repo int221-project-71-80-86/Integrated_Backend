f3fb5a62337d5c9464e9c46e75f2626f1631f310
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-05-01 13:35:22 +0700

    Create Service for store file

---
+++ src/main/java/int221/project/services/FileStoreServices.java
@@ -0,0 +1,20 @@
+package int221.project.services;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+
+public interface FileStoreServices {
+	public void init();
+	
+	public void save(MultipartFile file);
+		
+	public Resource load(String filename);
+	
+	public void deleteAll();
+	
+	public Stream<Path> loadAll();
+}
