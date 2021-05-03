182ef1d5338ccab6151c8dd590f96c87135d657b
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-05-03 22:28:22 +0700

    Push New Products Class. (remove mark as unchange)

--- src/main/java/int221/project/models/Products.java
+++ src/main/java/int221/project/models/Products.java
@@ -1,87 +1,147 @@
 package int221.project.models;
 
+import java.io.Serializable;
 import java.util.Date;
+import java.util.Set;
+import java.util.TreeSet;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
-public class Products
+public class Products implements Comparable<Products>,Serializable
 {
+	private static final long serialVersionUID = 1L;
+
 	@Id
-    private String     productcode  ; // Id or Primary Key
+	@Column(name = "productcode")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer productcode; // Id or Primary Key
 
-    private String name ;
-    private String desciption ;
-    private double price ;
-    private Date date ;
-    private String url ;
-    private double warranty ;
-    private int brandid ;
+    private String name;
+    private String description;
+    private Double price;
     
+    @Temporal(TemporalType.DATE)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+    private String image;
+    private Integer warranty;
+    private Integer brandid;
 
-	public void setProductcode( String productcode ) {
-        this.productcode = productcode ;
-    }
+    @OneToMany(mappedBy = "products", fetch=FetchType.LAZY, cascade = {CascadeType.ALL})
+    Set<Productcolors> productcolors = new TreeSet<>();
+    
+	public Set<Productcolors> getProductcolors() {
+		return productcolors;
+	}
 
-	public String getProductcode() {
-        return this.productcode;
-    }
+	public void setProductcolors(Set<Productcolors> productcolors) {
+		this.productcolors = productcolors;
+	}
 
-    public void setName( String name ) {
-        this.name = name;
-    }
+	public Integer getProductcode() {
+		return productcode;
+	}
 
-    public String getName() {
-        return this.name;
-    }
+	public void setProductcode(Integer productcode) {
+		this.productcode = productcode;
+	}
 
-    public void setDesciption( String desciption ) {
-        this.desciption = desciption;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getDesciption() {
-        return this.desciption;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setPrice( double price ) {
-        this.price = price;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public double getPrice() {
-        return this.price;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void setDate( Date date ) {
-        this.date = date;
-    }
+	public Double getPrice() {
+		return price;
+	}
 
-    public Date getDate() {
-        return this.date;
-    }
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
-    public void setUrl( String url ) {
-        this.url = url;
-    }
+	public Date getDate() {
+		return date;
+	}
 
-    public String getUrl() {
-        return this.url;
-    }
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
-    public void setWarranty( double warranty ) {
-        this.warranty = warranty;
-    }
+	public String getImage() {
+		return image;
+	}
 
-    public double getWarranty() {
-        return this.warranty;
-    }
+	public void setImage(String image) {
+		this.image = image;
+	}
 
-    public void setBrandid( int brandid ) {
-        this.brandid = brandid;
-    }
+	public Integer getWarranty() {
+		return warranty;
+	}
 
-    public int getBrandid() {
-        return this.brandid;
-    }
+	public void setWarranty(Integer warranty) {
+		this.warranty = warranty;
+	}
 
-}
+	public Integer getBrandid() {
+		return brandid;
+	}
+
+	public void setBrandid(Integer brandid) {
+		this.brandid = brandid;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((productcode == null) ? 0 : productcode.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Products other = (Products) obj;
+		if (productcode == null) {
+			if (other.productcode != null)
+				return false;
+		} else if (!productcode.equals(other.productcode))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Products o) {
+		return this.getProductcode() - o.getProductcode();
+	}
+    
+	}
