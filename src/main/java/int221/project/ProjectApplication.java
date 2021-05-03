444b7ccd674d0bb982e4ebb150c9dddae43114ba
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-05-03 12:00:42 +0700

    Clean comment code.

--- src/main/java/int221/project/ProjectApplication.java
+++ src/main/java/int221/project/ProjectApplication.java
@@ -9,19 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import int221.project.services.FileStoreServices;
 
 @SpringBootApplication
-//public class ProjectApplication implements CommandLineRunner {
 public class ProjectApplication {
-//	@Resource
-//	FileStoreServices storeService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 	
-//	@Override
-//	  public void run(String... arg) throws Exception {
-//	    storeService.deleteAll();
-//	    storeService.init();
-//	  }
-
 }
