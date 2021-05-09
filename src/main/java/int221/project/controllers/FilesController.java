7ffb7b32aa4733e367f33531781114870b7e5e54
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-05-09 15:14:36 +0700

    Remove unused method & imports.

--- src/main/java/int221/project/controllers/FilesController.java
+++ src/main/java/int221/project/controllers/FilesController.java
@@ -12,14 +12,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import int221.project.files.FileInfo;
-import int221.project.files.ResponseMessage;
 import int221.project.services.FileStoreServices;
 
 @Controller
@@ -28,22 +24,6 @@ public class FilesController {
 
 	@Autowired
 	FileStoreServices storeService;
-
-	///////////////////
-	// Upload Method //
-	///////////////////
-	@PostMapping("/upload")
-	public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
-		String message = "";
-		try {
-			String filename = storeService.save(file);
-			message = "Uploaded the file successfully: " + filename;
-			return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
-		} catch (Exception e) {
-			message = "Could not upload the file: " + file.getOriginalFilename() + "!";
-			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
-		}
-	}
 
 	//////////////////
 	// Get All File //
